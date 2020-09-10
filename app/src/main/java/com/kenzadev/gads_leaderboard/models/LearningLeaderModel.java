55de96be95d8456efa4a76dd8d7fa41eb9132f9e
Y-Kenza <hk_yousfi@esi.dz>
2020-09-11 00:31:42 +0100

    Minor changes

--- app/src/main/java/com/kenzadev/gads_leaderboard/models/LearningLeaderModel.java
+++ app/src/main/java/com/kenzadev/gads_leaderboard/models/LearningLeaderModel.java
@@ -17,33 +17,23 @@ public class LearningLeaderModel implements Comparable<LearningLeaderModel> {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public int getHours() {
         return hours;
     }
 
-    public void setHours(int hours) {
-        this.hours = hours;
-    }
+
 
     public String getCountry() {
         return country;
     }
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
+
 
     public String getBadgeUrl() {
         return badgeUrl;
     }
 
-    public void setBadgeUrl(String badgeUrl) {
-        this.badgeUrl = badgeUrl;
-    }
 
     @Override
     public int compareTo(LearningLeaderModel learningLeaderModel) {
--- app/src/main/java/com/kenzadev/gads_leaderboard/models/SkillIQModel.java
+++ app/src/main/java/com/kenzadev/gads_leaderboard/models/SkillIQModel.java
@@ -17,32 +17,16 @@ public class SkillIQModel implements Comparable<SkillIQModel> {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getScore() {
         return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
     }
 
     public String getCountry() {
         return country;
     }
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
     public String getBadgeUrl() {
         return badgeUrl;
-    }
-
-    public void setBadgeUrl(String badgeUrl) {
-        this.badgeUrl = badgeUrl;
     }
 
     @Override
