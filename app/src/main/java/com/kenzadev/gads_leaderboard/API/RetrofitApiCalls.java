f94152b11b295a68d178e0a6b18041b70618a191
Y-Kenza <hk_yousfi@esi.dz>
2020-09-10 21:43:52 +0100

    Completed Submission

---
+++ app/src/main/java/com/kenzadev/gads_leaderboard/API/RetrofitApiCalls.java
@@ -0,0 +1,30 @@
+package com.kenzadev.gads_leaderboard.API;
+
+import com.kenzadev.gads_leaderboard.models.LearningLeaderModel;
+import com.kenzadev.gads_leaderboard.models.SkillIQModel;
+
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface RetrofitApiCalls {
+
+    @GET("/api/hours")
+    Call<List<LearningLeaderModel>> getLearningLeaders();
+
+    @GET("/api/skilliq")
+    Call<List<SkillIQModel>> getSkillLeaders();
+
+    @FormUrlEncoded
+    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
+    Call<ResponseBody> submitGoogleForm(@Field("entry.1824927963") String email_address,
+                                        @Field("entry.1877115667") String first_name,
+                                        @Field("entry.2006916086") String last_name,
+                                        @Field("entry.284483984") String project_link);
+
+}
--- app/src/main/java/com/kenzadev/gads_leaderboard/ui/SubmitFormActivity.java
+++ app/src/main/java/com/kenzadev/gads_leaderboard/ui/SubmitFormActivity.java
@@ -8,15 +8,23 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.button.MaterialButton;
+
 import com.google.android.material.textfield.TextInputEditText;
+import com.kenzadev.gads_leaderboard.API.RetrofitApiCalls;
+import com.kenzadev.gads_leaderboard.API.RetrofitClient;
 import com.kenzadev.gads_leaderboard.R;
 
 import java.util.Objects;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SubmitFormActivity extends AppCompatActivity {
 
@@ -63,11 +71,8 @@ public class SubmitFormActivity extends AppCompatActivity {
         finish();
     }
 
-    private void inputValidations() {
+    private boolean isInputEmpty() {
         String cannotBeBlank = "Input cannot be blank";
-        String mustBeValidEmail = "Enter a valid email";
-        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-        String emailPattern2 = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+\\.+[a-z]+";
 
         firstName = Objects.requireNonNull(inputFirstName.getText()).toString();
         lastName = Objects.requireNonNull(inputLastName.getText()).toString();
@@ -75,22 +80,109 @@ public class SubmitFormActivity extends AppCompatActivity {
         githubLink = Objects.requireNonNull(inputGithubLink.getText()).toString();
         if (firstName.isEmpty()) {
             inputFirstName.setError(cannotBeBlank);
-            return;
+            return true;
         }
         if (lastName.isEmpty()) {
             inputLastName.setError(cannotBeBlank);
+            return true;
         }
         if (emailAddress.isEmpty()) {
             inputEmail.setError(cannotBeBlank);
+            return true ;
         }
         if (githubLink.isEmpty()) {
             inputGithubLink.setError(cannotBeBlank);
+            return true;
         }
-        if (!(emailAddress.matches((emailPattern)) || emailAddress.matches(emailPattern2))) {
-            inputEmail.setError(mustBeValidEmail);
-        }
+        return false;
+    }
 
-        //Alert Dialog
+    private void inputValidations() {
+        if (isInputEmpty())
+        {
+            Toast.makeText(SubmitFormActivity.this,"Please fill all inputs", Toast.LENGTH_LONG).show();
+        }
+        else {
+            //Are you sure? Dialog
+            Rect displayRectangle = new Rect();
+            Window window = SubmitFormActivity.this.getWindow();
+            window.getDecorView().getWindowVisibleDisplayFrame(displayRectangle);
+            final AlertDialog.Builder builder = new AlertDialog.Builder(
+                    SubmitFormActivity.this, R.style.dialog);
+            ViewGroup viewGroup = findViewById(android.R.id.content);
+
+            View view = LayoutInflater
+                    .from(SubmitFormActivity.this)
+                    .inflate(R.layout.dialog_are_you_sure, viewGroup, false);
+
+            view.setMinimumWidth((int) (displayRectangle.width() * 1f));
+            builder.setView(view);
+
+
+            final AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
+            Button approveButton = view.findViewById(R.id.approveButton);
+            ImageButton cancelDialog = view.findViewById(R.id.cancelSubmissionButton);
+
+            //if the user confirms
+            approveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    submitProject();
+                    alertDialog.dismiss();
+                }
+            });
+            //if the user cancels
+            cancelDialog.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    alertDialog.dismiss();
+                }
+            });
+        }
+    }
+
+    private void submitProject() {
+        RetrofitApiCalls google_service = RetrofitClient.getGoogleFormsRetrofitInstance().create(RetrofitApiCalls.class);
+        Call<ResponseBody> call = google_service.submitGoogleForm(emailAddress, firstName, lastName, githubLink);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.code() == 200) {
+                    //Successful
+                    showSuccessDialog();
+                } else {
+                    showFailDialog();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                //Not successful
+                showFailDialog();
+            }
+        });
+    }
+
+    public void showSuccessDialog() {
+        Rect displayRectangle = new Rect();
+        Window window = SubmitFormActivity.this.getWindow();
+        window.getDecorView().getWindowVisibleDisplayFrame(displayRectangle);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(
+                SubmitFormActivity.this, R.style.dialog);
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+        View view = LayoutInflater.from(SubmitFormActivity.this)
+                .inflate(R.layout.dialog_success, viewGroup,
+                        false);
+        view.setMinimumWidth((int) (displayRectangle.width() * 1f));
+        builder.setView(view);
+
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    public void showFailDialog() {
         Rect displayRectangle = new Rect();
         Window window = SubmitFormActivity.this.getWindow();
         window.getDecorView().getWindowVisibleDisplayFrame(displayRectangle);
@@ -99,34 +191,10 @@ public class SubmitFormActivity extends AppCompatActivity {
         ViewGroup viewGroup = findViewById(android.R.id.content);
         View view = LayoutInflater
                 .from(SubmitFormActivity.this)
-                .inflate(R.layout.dialog_are_you_sure, viewGroup, false);
-
+                .inflate(R.layout.dialog_not_successful, viewGroup, false);
         view.setMinimumWidth((int) (displayRectangle.width() * 1f));
         builder.setView(view);
-
         final AlertDialog alertDialog = builder.create();
         alertDialog.show();
-
-        MaterialButton approveButton = view.findViewById(R.id.confirmProjectSubmissionButton);
-        ImageButton cancelDialog = view.findViewById(R.id.cancelSubmissionButton);
-
-        approveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                submitProject();
-                alertDialog.dismiss();
-            }
-        });
-        cancelDialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                alertDialog.dismiss();
-            }
-        });
-
-    }
-
-    private void submitProject() {
-
     }
 }
